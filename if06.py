46dbd099fd2a88b6740dabf5a0ebfa44cec62028
azimabdurasulov <azimjon20050604@gmail.com>
2022-10-10 12:14:35 +0500

    if0

--- if06.py
+++ if06.py
@@ -13,4 +13,30 @@ def main(a,b,c):
     Returns:
         string: string with the result
     """
-    return
+    count0 = 0 
+    count1 = 0
+     
+    if a > 0:
+        count1 += 1
+    else:
+        count0 += 1
+
+    if b > 0:
+        count1 += 1
+    else:
+        count0 += 1
+
+    if c > 0:
+        count1 += 1
+    else:
+        count0 += 1
+
+
+    if count0 < count1:
+        print("there are a lot of positive numbers")
+    else:
+        print("there are a lot of negative numbers")
+
+    return count0, count1
+
+print(main(32, 4, -76))
