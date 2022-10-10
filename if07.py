e1c4a91cffed1ae81b55450d743b0c80923f368f
azimabdurasulov <azimjon20050604@gmail.com>
2022-10-10 12:48:01 +0500

    if07.py

--- if07.py
+++ if07.py
@@ -12,4 +12,21 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if a % 2 == 1 and a > 0:
+        print("positive odd number", a)
+    
+    if a % 2 == 0 and a > 0:
+        print("positive even number", a)
+
+    if a % 2 == 1 and a < 0:
+        print("negative odd number", a)
+
+    if a % 2 == 0 and a < 0:
+        print("negative even number", a)
+
+
+    if a == 0: 
+        print("the number is zero", a)
+    return a
+
+print(main(44))
